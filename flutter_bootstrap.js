c859d09702ba1cb99bfe3e5f86aa667578bf5afd
Alekla0126 <Alekla0126@users.noreply.github.com>
2024-12-27 23:24:58 +0000

    deploy: 5d9f19bf48e61c562f190da1b46425f88185c6e4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "928504808"
+    serviceWorkerVersion: "911318740"
   }
 });
